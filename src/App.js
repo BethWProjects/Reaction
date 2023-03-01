1733325dc2cd2c2c3606c2649bbb7d792657aa1f
Beth Wilson <bethwilson777@gmail.com>
2023-03-01 15:39:44 -0700

    Delete useEffect used for debugging messages coming through

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
   // import initialState as an object of message key with an empty array value
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  //for debuggin purposes, set up a useEffect hook to check the messages coming through.  This takes in a callback function to fire every single render, (or what it's set to) with the setInterval method.  within the setInterval method we have a callback function that calls a dispatch, calling a newMessage action creator
-  useEffect(() => {
-    setInterval(
-      () => dispatch(newMessage('foo')),
-      10000
-      );
-  }, []);
-
   console.log('state', state)
 
   return (
